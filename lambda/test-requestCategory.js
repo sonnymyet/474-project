65ceca0736c3f69529d94c79bf608befb8ad1887
sonny <shm6@sfu.ca>
2020-12-01 13:58:06 -0800

    Merged

--- lambda/test-requestCategory.js
+++ lambda/test-requestCategory.js
@@ -1,19 +1,19 @@
 {
-    "path": "/home",
-    "httpMethod": "GET",
-    "headers": {
-        "Accept": "*/*",
-        "Authorization": "eyJraWQiOiJLTzRVMWZs",
-        "content-type": "application/json; charset=UTF-8"
-    },
-    "queryStringParameters": null,
-    "pathParameters": null,
-    "requestContext": {
-        "authorizer": {
-            "claims": {
-                "cognito:username": "the_username"
-            }
-        }
-    },
-    "body": "{\"Category\":{\"Id\": 0,\"Title\":\"Test Title\"}}"
+  "path": "/home",
+  "httpMethod": "GET",
+  "headers": {
+    "Accept": "*/*",
+    "Authorization": "eyJraWQiOiJLTzRVMWZs",
+    "content-type": "application/json; charset=UTF-8"
+  },
+  "queryStringParameters": null,
+  "pathParameters": null,
+  "requestContext": {
+    "authorizer": {
+      "claims": {
+        "cognito:username": "the_username"
+      }
+    }
+  },
+  "body": "{\"Category\":{\"Id\": 5,\"Title\":\"Test Title\"}}"
 }
